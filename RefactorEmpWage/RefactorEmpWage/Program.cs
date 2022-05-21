3548de5916fb64987accb90e32c4d0634d7d9b85
Kundan <kundan kamble>
2022-05-20 23:56:34 -0700

    [KUNDAN] Added refactor code program

---
+++ RefactorEmpWage/RefactorEmpWage/Program.cs
@@ -0,0 +1,71 @@
+﻿//UC7
+
+
+using System;
+
+namespace CalculateEmployeeWage
+{
+    class EmployeeWageComputaion
+    {
+        private int _empRatePrHr;
+        private const int _isFullTime = 1;
+        private const int _isPartTime = 2;
+        private int _fullTimeHrs;
+        private int _partTimeHrs;
+        private int _numOfDays;
+        private int _maxHrs;
+        private int totalWage;
+
+        public EmployeeWageComputaion(int empRatePrHr, int fullTimeHrs, int partTimeHrs, int maxdays, int maxHours)
+        {
+            _empRatePrHr = empRatePrHr;
+            _fullTimeHrs = fullTimeHrs;
+            _partTimeHrs = partTimeHrs;
+            _numOfDays = maxdays;
+            _maxHrs = maxHours;
+        }
+
+        private int IsEmployeePresent()
+        {
+            return new Random().Next(0, 3);
+        }
+
+        public void CalculateWage()
+        {
+            int workHr;
+            int totalWrkHr = 0;
+            int totalWrkDays = 0;
+
+            while (totalWrkHr < _maxHrs && totalWrkDays < _numOfDays)
+            {
+                switch (IsEmployeePresent())
+                {
+                    case _isFullTime:
+                        workHr = _fullTimeHrs;
+                        break;
+                    case _isPartTime:
+                        workHr = _partTimeHrs;
+                        break;
+                    default:
+                        workHr = 0;
+                        break;
+                }
+                totalWrkHr += workHr;
+                totalWrkDays++;
+            }
+            totalWage = totalWrkHr * _empRatePrHr;
+            Console.WriteLine("Employee total wage is {0} for {1} working days", totalWage, totalWrkDays);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation!");
+            Console.WriteLine();
+            EmployeeWageComputaion wage = new EmployeeWageComputaion(20, 8, 4, 20, 100);
+            wage.CalculateWage();
+        }
+    }
+}
