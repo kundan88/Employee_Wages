172a1de633ad798e0caf0294335a3e480203bb94
Kundan <kundan kamble>
2022-05-20 23:37:20 -0700

    [KUNDAN] Added Employee wage program

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculateEmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            int isPresent = 1;
+            int employeeStatus = new Random().Next(0, 2);
+
+            if (employeeStatus == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
