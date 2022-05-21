c4d219ee1d12ab6524dc4e81bc322eac3419c27d
Kundan <kundan kamble>
2022-05-20 23:52:07 -0700

    [KUNDAN] Added Calculate wage for month program

---
+++ EmpMonthlyWage/EmpMonthlyWage/Program.cs
@@ -0,0 +1,41 @@
+﻿//UC5
+using System;
+
+namespace CalculateEmployeeWage
+{
+    internal class Program
+    {
+        public const int empRatePrHr = 20;
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int numOfDays = 20;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            int workHr;
+            int empWage;
+            int totalWage = 0;
+            for (int days = 0; days < numOfDays; days++)
+            {
+                int employeeStatus = new Random().Next(0, 2);
+                switch (employeeStatus)
+                {
+                    case isFullTime:
+                        workHr = 8;
+                        break;
+                    case isPartTime:
+                        workHr = 4;
+                        break;
+                    default:
+                        workHr = 0;
+                        break;
+                }
+                empWage = empRatePrHr * workHr;
+                totalWage += empWage;
+            }
+            Console.WriteLine("Employee total wage is " + totalWage);
+        }
+    }
+}
