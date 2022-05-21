02f32c999972e9ff0c52814c84e143e81f2318eb
Kundan <kundan kamble>
2022-05-20 23:54:39 -0700

    [KUNDAN] Added calculate wages till condition program

---
+++ CalWagesCondition/CalWagesCondition/Program.cs
@@ -0,0 +1,46 @@
+﻿//UC6
+
+using System;
+
+namespace CalculateEmployeeWage
+{
+    internal class Program
+    {
+        public const int empRatePrHr = 20;
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int numOfDays = 20;
+        public const int maxHrs = 100;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            int workHr;
+            int totalWrkHr = 0;
+            int totalWage;
+            int totalWrkDays = 0;
+            while (totalWrkHr < maxHrs && totalWrkDays < numOfDays)
+            {
+                int employeeStatus = new Random().Next(0, 2);
+                switch (employeeStatus)
+                {
+                    case isFullTime:
+                        workHr = 8;
+                        break;
+                    case isPartTime:
+                        workHr = 4;
+                        break;
+                    default:
+                        workHr = 0;
+                        break;
+                }
+                totalWrkHr += workHr;
+                totalWrkDays++;
+            }
+            totalWage = totalWrkHr * empRatePrHr;
+            Console.WriteLine("Employee total wage is " + totalWage
+                + " for {0} working Days", totalWrkDays);
+        }
+    }
+}
