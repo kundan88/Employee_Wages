23a7bd274890aae64e6e819ade297f09d4621533
Kundan <kundan kamble>
2022-05-20 23:49:10 -0700

    [KUNDAN] Added Using SwitchCase program

---
+++ EmpWageUsingSwitchCase/EmpWageUsingSwitchCase/Program.cs
@@ -0,0 +1,38 @@
+﻿//UC4
+
+using System;
+
+namespace CalculateEmployeeWage
+{
+    internal class Program
+    {
+        public const int empRatePrHr = 20;
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            int workHr;
+            int totalWage;
+
+            int employeeStatus = new Random().Next(0, 2);
+
+            switch (employeeStatus)
+            {
+                case isFullTime:
+                    workHr = 8;
+                    break;
+                case isPartTime:
+                    workHr = 4;
+                    break;
+                default:
+                    workHr = 0;
+                    break;
+            }
+            totalWage = empRatePrHr * workHr;
+            Console.WriteLine("Employee total wage is " + totalWage);
+        }
+    }
+}
