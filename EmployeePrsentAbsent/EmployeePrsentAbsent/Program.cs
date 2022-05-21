5d163b4be1e5fbd884f77905fe2d0449d3fc3425
Kundan <kundan kamble>
2022-05-20 23:40:39 -0700

    [KUNDAN] Added present and absent program

---
+++ EmployeePrsentAbsent/EmployeePrsentAbsent/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//UC1
+using System;
+
+namespace CalculateEmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            int isPresent = 1;               
+            int employeeStatus = new Random().Next(0, 2);
+
+            if (employeeStatus == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
