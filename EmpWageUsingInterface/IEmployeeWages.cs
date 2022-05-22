33070ab0c38a1d8037897143fa640d57f976391e
Kundan <kundan kamble>
2022-05-22 16:34:08 -0700

    [KUNDAN] Added Employee wage using interface program

---
+++ EmpWageUsingInterface/IEmployeeWages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateEmployeeWage
+{
+    internal interface IEmployeeWage
+    {
+        void AddCompany(string CompanyName, int EmpWagePrHr, int FullTimeWrkHr, int PartTimeWrkHr,
+            int MaxWrkHrs, int MaxWrkDays);
+        int CalculateWage(string CompanyName);
+    }
+}
---
+++ EmpWageUsingInterface/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CalculateEmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to Employee Wage Computation!");
+            Console.WriteLine();
+            EmployeeWageComputaion wage = new EmployeeWageComputaion();
+            wage.AddCompany("TATA", 20, 8, 4, 100, 20);
+            wage.AddCompany("Mahindra", 30, 8, 4, 100, 40);
+            wage.AddCompany("Microsoft", 100, 8, 4, 100, 30);
+
+            
+            int[] wages = new int[3];
+            wages[0] = wage.CalculateWage("Microsoft");
+            wages[1] = wage.CalculateWage("Tata");
+            wages[2] = wage.CalculateWage("Mahindra");
+
+            foreach (var item in wages)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
+
